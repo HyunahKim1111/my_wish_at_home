0776f4bed81536a765d59e9694d826f094cb7945
HyunahKim1111 <wingsoul11@naver.com>
2023-12-17 00:39:20 +0900

    author필드추가하기

--- wish_content/models.py
+++ wish_content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 #포스트 메인 페이지
@@ -14,9 +15,11 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     time = models.DateField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # 관리자페이지에서 글을 문자로 뿌려주는 역할
     def __str__(self):
-        return f'[{self.pk}]{self.title}{self.time}'
+        return f'[{self.pk}]{self.title}{self.time} :: {self.author}'
     #메인페이지에서 제목을 클릭하면 상세페이지로 넘어가는 url
     def get_absolute_url(self):
         return f'/{self.pk}/'
